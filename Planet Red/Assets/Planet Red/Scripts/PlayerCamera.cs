94cc2002b78e6ce1407c87fae332dabbf6a474a5
Joshua George <joshuageorge276@gmail.com>
2018-07-30 22:15:40 +0100

    Added Player Camera
    Camera will follow a specified target.

---
+++ Planet Red/Assets/Planet Red/Scripts/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public GameObject target;
+    [Range(0.0f, 1.0f)]
+    public float followSmoothing = 0.3f;
+
+    private Vector3 velocity;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + offset, ref velocity, followSmoothing);
+    }
+}
