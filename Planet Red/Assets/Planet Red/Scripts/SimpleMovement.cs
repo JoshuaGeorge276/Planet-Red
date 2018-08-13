1bbedd53b030982309b2d0df208f8f24804bf4af
Joshua George <joshuageorge276@gmail.com>
2018-08-13 21:54:43 +0100

    Added a Simple Animation System and Character

--- Planet Red/Assets/Planet Red/Scripts/SimpleMovement.cs
+++ Planet Red/Assets/Planet Red/Scripts/SimpleMovement.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class SimpleMovement : MonoBehaviour {
 
-    public float speed = 5.0f; 
+    private int ANIM_RUNNING = Animator.StringToHash("Running");
+
+    public float speed = 5.0f;
+    public float rotationSmoothing = 0.3f;
+
+    private CharacterController character;
+    private Animator anim;
+
+    private Vector3 movementDirection;
+    private Vector3 zeroVector;
 
 	// Use this for initialization
 	void Start () {
-		
+        character = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();	
 	}
 	
 	// Update is called once per frame
@@ -18,6 +28,29 @@ public class SimpleMovement : MonoBehaviour {
 
         Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
 
-        transform.position += (direction.normalized * speed * Time.deltaTime);
+        movementDirection = direction * speed * Time.deltaTime;
+
+        Move();
+        Rotate();
+
+        anim.SetFloat(ANIM_RUNNING, direction.normalized.sqrMagnitude);
+
 	}
+
+    private void Move()
+    {
+        character.Move(movementDirection);
+    }
+
+    private void Rotate()
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(movementDirection);
+
+
+        if (transform.rotation != lookRotation && movementDirection != zeroVector)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSmoothing);
+        }
+       
+    }
 }
